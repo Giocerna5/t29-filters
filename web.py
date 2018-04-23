f65ef44b6a937cfba3de33f551d3a1b5ccca9385
Giocerna5 <gcerna@csumb.edu>
2018-04-23 13:51:11 -0700

    update

--- web.py
+++ web.py
@@ -1,1 +1,9 @@
-print('hello')
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
+
+@app.route('/')
+def home():
+    return render_template('template.html')
